aa460a3446fa1ead38516547ac7fb9c4b68a8133
vahvake <62074587+Vahvelainen@users.noreply.github.com>
2024-01-17 21:25:19 +0100

    Make opening images faster

--- prototype/tools.py
+++ prototype/tools.py
@@ -12,16 +12,6 @@ def openFileInDefaultProgram(filepath):
       subprocess.call(('xdg-open', filepath))
 
 def openAndResizeImage(image_path, target_width, target_height):
-    image = Image.open(image_path) 
-    width, height = image.size
-    # Determine the factor to scale down while maintaining the aspect ratio.
-    scale_factor = min(target_width / width, target_height / height)
-
-    # Check if the scale factor is less than 1 (image is larger than the label).
-    if scale_factor < 1:
-        # Resize the image while maintaining the aspect ratio.
-        new_width = int(width * scale_factor)
-        new_height = int(height * scale_factor)
-        image = image.resize((new_width, new_height))
-        
+    image = Image.open(image_path)
+    image.thumbnail((target_width, target_height))
     return image
